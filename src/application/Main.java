fca33cded96798d4ec9cbda08fec510818adf9b6
Garrett Lyssy <gglyssy@gmail.com>
2021-03-27 16:41:40 -0500

    Put MenuItem

--- src/application/Main.java
+++ src/application/Main.java
@@ -25,33 +25,12 @@ public class Main extends Application {
 		primaryStage.show();
 	}
 	
-	static int array[] = IntStream.range(0, 10).toArray();
-	
-	public static void shuffleArray(int[] array) {
-	    int index;
-	    Random random = new Random();
-	    for (int i = array.length - 1; i > 0; i--)
-	    {
-	        index = random.nextInt(i + 1);
-	        if (index != i)
-	        {
-	            array[index] ^= array[i];
-	            array[i] ^= array[index];
-	            array[index] ^= array[i];
-	        }
-	    }
-	}
-	
 	public static Stage getStage() {
 		return primaryStage;
 	}
 	
 	public static void main(String[] args) {		
 		Application.launch(args);
-		shuffleArray(array);
-		for(int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
-		}
 	}
 	public static int NumOfQuestions;
 	public static String CategorySelected = "";
--- src/controller/MainScreenController.java
+++ src/controller/MainScreenController.java
@@ -12,20 +12,61 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
 public class MainScreenController {
 
 	private Stage primaryStage;
 	
-    @FXML
+	@FXML
     public Button StartAGame;
-    
-    @FXML
-    public Button HowToPlay;
 
     @FXML
-    public Button About;
+    public MenuItem HTP;
+
+    @FXML
+    public MenuItem Home;
+
+    @FXML
+    public MenuItem Settings;
+
+    @FXML
+    public MenuItem About;
+    
+    
+    @FXML
+    public void goHome(ActionEvent event) throws IOException {
+    	FXMLLoader main = new FXMLLoader(getClass().getResource("/view/MainScreen.fxml"));
+		Parent root = main.load();
+		Stage stage = Main.getStage();
+		stage.setScene(new Scene(root));
+		stage.show();
+    }
+
+    @FXML
+    public void SettingsOpened(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void HTPPushed(ActionEvent event) throws IOException {
+    	FXMLLoader HTP = new FXMLLoader(getClass().getResource("/view/HowToPlayInGame.fxml"));
+		Parent root = HTP.load();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+		stage.setTitle("How To Play");
+		stage.show();
+    }
+
+    @FXML
+    public void openAbout(ActionEvent event) throws IOException {
+    	FXMLLoader about = new FXMLLoader(getClass().getResource("/view/AboutScreen.fxml"));
+		Parent root = about.load();
+		Stage stage = Main.getStage();
+		stage.setScene(new Scene(root));
+		stage.show();
+    }
     
     @FXML
     public void StartAGamePressed(ActionEvent event) throws IOException {
@@ -34,27 +75,6 @@ public class MainScreenController {
 		Stage stage = Main.getStage();
 		stage.setScene(new Scene(root));
 		stage.show();
-    }
-
-    @FXML
-    public void HowToPlayPressed(ActionEvent event) throws IOException {
-    	FXMLLoader HTP = new FXMLLoader(getClass().getResource("/view/HowToPlayScreen.fxml"));
-		Parent root = HTP.load();
-		Stage stage = Main.getStage();
-		stage.setScene(new Scene(root));
-		stage.show();
-    }
-
-    @FXML
-    public void AboutPressed(ActionEvent event) throws IOException {
-			FXMLLoader about = new FXMLLoader(getClass().getResource("/view/AboutScreen.fxml"));
-			Parent root = about.load();
-			Stage stage = Main.getStage();
-			stage.setScene(new Scene(root));
-			stage.show();
-		
-    }
-    
-    
+    }   
 
 }
