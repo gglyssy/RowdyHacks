9194f4dfc536b935447ac37494b67cf6075ddf78
Garrett Lyssy <gglyssy@gmail.com>
2021-03-27 00:58:34 -0500

    New updates to controllers
    Little changes

--- src/controller/HowToPlayScreenController.java
+++ src/controller/HowToPlayScreenController.java
@@ -1,5 +1,17 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class HowToPlayScreenController {
+
+    @FXML
+    public Button GoBack;
+
+    @FXML
+    public void GoBackPressed(ActionEvent event) {
+
+    }
 
 }
