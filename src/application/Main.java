9e27ef2621d4362d7fd42b12e514f2a3ab9dfe53
Garrett Lyssy <gglyssy@gmail.com>
2021-03-26 22:27:13 -0500

    Initial

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 		try {
 			Parent root=FXMLLoader.load(getClass().getResource("/view/MainScreen.fxml"));
 			Scene scene = new Scene(root);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
