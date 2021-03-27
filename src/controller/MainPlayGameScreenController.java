167941c6b71b76f5e1676ae200e185afee035c73
Garrett Lyssy <gglyssy@gmail.com>
2021-03-27 13:07:49 -0500

    Yes

--- src/controller/MainPlayGameScreenController.java
+++ src/controller/MainPlayGameScreenController.java
@@ -14,6 +14,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 import model.Categories;
@@ -39,6 +40,12 @@ public class MainPlayGameScreenController {
 
     @FXML
     public MenuItem HTP;
+    
+    @FXML
+    public Label QuestionTitle;
+
+    @FXML
+    public Label SolutionTitle;
     
     HashMap<Categories, ArrayList<Questions>> questionMap = new HashMap<Categories, ArrayList<Questions>>();
     
